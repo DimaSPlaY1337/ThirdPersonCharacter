360314bfc2dd074d714f3f0aa773e0c59f5bc33c
IvanIv99 <finrez205@gmail.com>
2024-06-05 20:58:43 +0300

    Base mechs

--- Source/Homework/PlayerCharacter.h
+++ Source/Homework/PlayerCharacter.h
@@ -12,60 +12,60 @@
 UCLASS(Blueprintable)
 class HOMEWORKPROJECT_API APlayerCharacter : public AGCBaseCharacter
 {
-GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-APlayerCharacter(const FObjectInitializer& ObjectInitializer);
+    APlayerCharacter(const FObjectInitializer& ObjectInitializer);
 
-virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
-virtual void MoveForward(float Value) override;
-virtual void MoveRight(float Value) override;
-virtual void Turn(float Value) override;
-virtual void LookUp(float Value) override;
-virtual void TurnAtRate(float Value) override;
-virtual void LookUpAtRate(float Value) override;
+    virtual void MoveForward(float Value) override;
+    virtual void MoveRight(float Value) override;
+    virtual void Turn(float Value) override;
+    virtual void LookUp(float Value) override;
+    virtual void TurnAtRate(float Value) override;
+    virtual void LookUpAtRate(float Value) override;
 
-virtual void OnStartCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust) override;
-virtual void OnEndCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust) override;
+    virtual void OnStartCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust) override;
+    virtual void OnEndCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust) override;
 
-virtual bool CanJumpInternal_Implementation() const override;
-virtual void OnJumped_Implementation() override;
+    virtual bool CanJumpInternal_Implementation() const override;
+    virtual void OnJumped_Implementation() override;
 
-virtual void OnStartProne(float HalfHeightAdjust);
-virtual void OnEndProne(float HalfHeightAdjust);
+    virtual void OnStartProne(float HalfHeightAdjust);
+    virtual void OnEndProne(float HalfHeightAdjust);
 
-virtual void OnStartSlide(float HalfHeightAdjust);
-virtual void OnEndSlide(float HalfHeightAdjust);
+    virtual void OnStartSlide(float HalfHeightAdjust);
+    virtual void OnEndSlide(float HalfHeightAdjust);
 
-virtual void SwimForward(float Value) override;
-virtual void SwimRight(float Value) override;
-virtual void SwimUp(float Value) override;
+    virtual void SwimForward(float Value) override;
+    virtual void SwimRight(float Value) override;
+    virtual void SwimUp(float Value) override;
 
-virtual void Tick(float DeltaTime) override;
-float GetForwardAxis() { return ForwardAxis; }
-float GetRightAxis() { return RightAxis; }
+    virtual void Tick(float DeltaTime) override;
+    float GetForwardAxis() { return ForwardAxis; }
+    float GetRightAxis() { return RightAxis; }
 
-UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Camera")
-class UCameraComponent* CameraComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Camera")
+        class UCameraComponent* CameraComponent;
 
-UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Camera")
-class USpringArmComponent* SpringArmComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Camera")
+        class USpringArmComponent* SpringArmComponent;
 
-void AimCameraUpdate(float Value);
+    void AimCameraUpdate(float Value);
 
 protected:
-virtual void OnStartAimingInternal() override;
-virtual void OnStopAimingInternal() override;
+    virtual void OnStartAimingInternal() override;
+    virtual void OnStopAimingInternal() override;
 
-UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character | AIM")
-UCurveFloat* AimCurve;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character | AIM")
+        UCurveFloat* AimCurve;
 
-void SetIsAiming(float Value);
+    void SetIsAiming(float Value);
 
 private:
-bool IsAiming;
-FTimeline AimingTimeline;
-float ForwardAxis;
-float RightAxis;
+    bool IsAiming;
+    FTimeline AimingTimeline;
+    float ForwardAxis;
+    float RightAxis;
 };
